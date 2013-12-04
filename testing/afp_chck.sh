b715e5c0e60adfebdf4c9c7e1a688d00bf087d50
Ben Bass <ben@benbass.com>
2013-12-03 23:08:26 -0500

    VPN checking
    Testing code to check VPN connections.  Needs to be added to trdaily.

--- testing/afp_chck.sh
+++ testing/afp_chck.sh
@@ -10,12 +10,15 @@ if [[ "$os_vers_chck" -le 107 ]]; then
 	srvr_vchck=22
 	afp_status_raw=$(/usr/sbin/serveradmin command afp:command = getConnectedUsers)
 	smb_status_raw=$(/usr/sbin/serveradmin command smb:command = getConnectedUsers)
+	vpn_status_raw=$(/usr/sbin/serveradmin command vpn:command = getConnectedUsers)
+
 fi	
 if [[ "$os_vers_chck" == 108 ]]; then
 	srvr_vchck_raw="$(defaults read /Applications/Server.app/Contents/Info CFBundleShortVersionString)"
 	srvr_vchck="$(echo "$srvr_vchck_raw" | sed 's/\.//g' | cut -c 1-2)"
 	afp_status_raw=$(/Applications/Server.app/Contents/ServerRoot/usr/sbin/serveradmin command afp:command = getConnectedUsers)
 	smb_status_raw=$(/Applications/Server.app/Contents/ServerRoot/usr/sbin/serveradmin command smb:command = getConnectedUsers)
+	vpn_status_raw=$(/Applications/Server.app/Contents/ServerRoot/usr/sbin/serveradmin command vpn:command = getConnectedUsers)
 fi	
 
 
@@ -101,7 +104,7 @@ if [[ -z "$srvr_vchck" ]]; then
 				echo "The SMB Service is currently "$smb_status""
 				else
 		# grab the raw data for AFP users, then get the usernames and IP's.
-		usname="$(echo "$smb_status_raw" | grep -i "name"| cut -f 2 -d = | sed 's/\"//g' | sed 's/ //g')"
+		usname="$(echo "$smb_status_raw" | grep -i "name" | cut -f 2 -d = | sed 's/\"//g' | sed 's/ //g')"
 		uip="$(echo "$smb_status_raw" | grep -i "ipaddress" | cut -f 2 -d = | sed 's/\"//g' | sed 's/ //g')"
 		ct="$(echo "$afp_status_raw" | grep -i "loginelapsedtime" | cut -f 2 -d = | sed 's/\"//g' | sed 's/ //g')"
 
@@ -143,6 +146,58 @@ if [[ -z "$srvr_vchck" ]]; then
 			echo "There are no users connected via SMB"
 		fi
 	fi
+	else 
+		echo "$srvr_log"
+fi
+#------------ VPN Check --------------------
+
+if [[ -z "$srvr_vchck" ]]; then
+	echo "$srvr_log"
+	elif [[ "$srvr_vchck" -ge 20 ]]; then
+		# grab the raw data for AFP users, then get the usernames and IP's.
+		usname="$(echo "$vpn_status_raw" | grep "name" | cut -f 2 -d = | sed 's/\"//g' | sed 's/ //g')"
+		protocol="$(echo "$vpn_status_raw" | grep "protocol" | cut -f 2 -d = | sed 's/\"//g' | sed 's/ //g')"
+		urip="$(echo "$vpn_status_raw" | grep -i "CommRemoteAddress" | cut -f 2 -d = | sed 's/\"//g' | sed 's/ //g')"
+		ulip="$(echo "$vpn_status_raw" | grep -i "privateAddress" | cut -f 2 -d = | sed 's/\"//g' | sed 's/ //g')"
+		ct="$(echo "$vpn_status_raw" | grep -i "connectionelapsedtime" | cut -f 2 -d = | sed 's/ //g' | cut -f 1 -d .)"
+
+		# Create an array for the usernames, IP's and connection time.  Each username will match with each IP, so element 1 in usname will 	correspond with uip element 1.
+		usname_array=($(echo $usname))
+		prot_array=($(echo $protocol))
+		urip_array=($(echo $urip))
+		ulip_array=($(echo $ulip))
+		ct_array=($(echo $ct))
+		usname_ct="$(echo "$vpn_status_raw" | grep -i "totalconnections" | cut -f 2 -d = | sed 's/ //g')"
+
+		# Put it together and make it pretty.
+		echo "The VPN Service currently has "$usname_ct" users connected"
+		echo ""
+		if [[ "$usname_ct" -gt 0 ]]; then
+			echo -e "Username"'\t''\t'"local IP"'\t''\t'"Remote IP"'\t''\t'"Protocol"'\t'"Connected For"
+			echo "-------------------------------------------------------------------------------------------------------------------"
+			# Number of items in the username array, so we know how many times to iterate through
+			# the count of items in each array should match - might add checking in later versions
+			# Iterate through each array - printing the username then corresponding IP.  
+			# using the IF statements for formatting purposes
+			for ((i=0;i<$usname_ct;i++)); do
+					cdiff=${ct_array[${i}]}
+					cdays=$(($cdiff/86400));
+					cdiff=$(($cdiff-($cdays*86400)))
+					chours=$(($cdiff/3600))
+					cdiff=$(($cdiff-($chours*3600)))
+					cminutes=$(($cdiff/60))
+					cseconds=$(($cdiff-($cminutes*60)))
+				if [[ ${#usname_array[${i}]} -ge 8 ]]; then	
+					echo -e "${usname_array[${i}]}"'\t'"${ulip_array[${i}]}"'\t''\t'"${urip_array[${i}]}"'\t''\t'"${prot_array[${i}]}"'\t''\t'`format "$cdays" "day"` `format "$chours" "hour"` `format "$cminutes" "minute"`
+					echo "-------------------------------------------------------------------------------------------------------------------"
+				else
+					echo -e "${usname_array[${i}]}"'\t''\t''\t'"${ulip_array[${i}]}"'\t''\t'"${urip_array[${i}]}"'\t''\t'"${prot_array[${i}]}"'\t''\t'`format "$cdays" "day"` `format "$chours" "hour"` `format "$cminutes" "minute"`
+					echo "-------------------------------------------------------------------------------------------------------------------"
+				fi
+			done
+		else
+			echo "There are no users connected via VPN"
+		fi
 	else 
 		echo "$srvr_log"
 fi
